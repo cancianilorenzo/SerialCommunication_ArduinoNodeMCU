7ae3d1c0e993807bf674fc5f89d1b68197d44e17
Lorenzo Canciani <76554746+cancianilorenzo@users.noreply.github.com>
2021-12-10 08:56:40 +0100

    Update NodeSide.ino

--- NodeSide/NodeSide.ino
+++ NodeSide/NodeSide.ino
@@ -1,9 +1,6 @@
 void setup() {
-  // Open serial communications and wait for port to open:
+  // Open serial communications:
   Serial.begin(115200);
-  /*while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }*/
 }
 void loop() { // run over and over
   if (Serial.available()) {
